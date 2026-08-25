de538059916170cbcd8b4f2f8097a6b63c5189b2
GoreeCloud <295260680+GoreeCloud@users.noreply.github.com>
2026-08-25 13:06:20 -0500

    Serve native Preferences script

--- native/internal/webui/webui.go
+++ native/internal/webui/webui.go
@@ -35,6 +35,10 @@ func Styles(w http.ResponseWriter, _ *http.Request) {
 	serveAsset(w, "assets/app.css", "text/css; charset=utf-8")
 }
 
+func PreferencesScript(w http.ResponseWriter, _ *http.Request) {
+	serveAsset(w, "assets/preferences.js", "text/javascript; charset=utf-8")
+}
+
 func ResultsStyles(w http.ResponseWriter, _ *http.Request) {
 	serveAsset(w, "assets/results.css", "text/css; charset=utf-8")
 }
